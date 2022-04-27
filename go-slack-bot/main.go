d3ffad59bec6fefacc8be0e9a5480dc482cd5c05
carlosayol <Carlosaolar@gmail.com>
2022-04-27 14:43:56 -0500

    add env file

--- go-slack-bot/main.go
+++ go-slack-bot/main.go
@@ -21,9 +21,6 @@ func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
 }
 
 func main() {
-	os.Setenv("SLACK_BOT_TOKEN", "xoxb-3471428882864-3444834871205-lEVk0F7mWRYrSY5Nn9LdjAy4")
-	os.Setenv("SLACK_APP_TOKEN", "xapp-1-A03CZ19DU30-3450038853828-dd963451c915d014f7fbc7dc5c382d0bf5748cae86e61b4f69f4d4e4514a014c")
-
 	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
 
 	go printCommandEvents(bot.CommandEvents())
