8645a7cf56892b68a2c8e5e67bbd9f0b39da3709
carlosayol <Carlosaolar@gmail.com>
2022-04-27 13:47:33 -0500

    create slack bot for born year calculation

---
+++ go-slack-bot/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/shomali11/slacker"
+)
+
+func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
+	for event := range analyticsChannel {
+		fmt.Println("Command Events")
+		fmt.Println(event.Timestamp)
+		fmt.Println(event.Command)
+		fmt.Println(event.Parameters)
+		fmt.Println(event.Event)
+	}
+}
+
+func main() {
+	os.Setenv("SLACK_BOT_TOKEN", "xoxb-3471428882864-3444834871205-lEVk0F7mWRYrSY5Nn9LdjAy4")
+	os.Setenv("SLACK_APP_TOKEN", "xapp-1-A03CZ19DU30-3450038853828-dd963451c915d014f7fbc7dc5c382d0bf5748cae86e61b4f69f4d4e4514a014c")
+
+	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
+
+	go printCommandEvents(bot.CommandEvents())
+
+	bot.Command("I was born in <year>", &slacker.CommandDefinition{
+		Description: "born year calculator",
+		Example:     "I was born in 1999",
+		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
+			year := request.Param("year")
+			yob, err := strconv.Atoi(year)
+
+			if err != nil {
+				println("error")
+			}
+			age := 2022 - yob
+			r := fmt.Sprintf("age is %d", age)
+			response.Reply(r)
+		},
+	})
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err := bot.Listen(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
